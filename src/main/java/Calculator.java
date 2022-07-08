32516dbff78218dcceec3cacae1d6ceb3a27d9b9
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-07 20:09:34 -0500

    Se modifica ejercicio haciendo uso de stream con map para sumar los elementos de dos listas

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,41 +1,113 @@
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Calculadora
+ *
+ * @author Óscar Farfán - oscarfarfan92@gmail.com
+ *
+ * @version 1.0.0.000 7-07-2022
+ */
 public class Calculator {
 
-    public Integer add(List<Integer> numbers){
-        Integer resultado = numbers.stream()
-                .sorted((x, y) -> x.compareTo(y))
-                .reduce((acumulator, number) -> {
-                    return acumulator + number;
-                }).get();
+    /**
+     * Método para sumar los elementos de dos listas del mismo tamaño,
+     * si las listas son de diferente tamaño devuelve un arreglo vacío
+     *
+     * @param list1 primera lista de tipo Double
+     * @param list2 segunda lista de tipo Double
+     * @return devuelve una nueva lista con los valores de cada item sumados
+     */
+    public List<Double> add(List<Double> list1, List<Double> list2){
+        if(list1.size() != list2.size()){return List.of();}
+        List<Double> resultado = list1.stream()
+                .map(element -> {
+                    Integer index = list1.indexOf(element);
+                    return element + list2.get(index);
+                }).collect(Collectors.toList());
+
         return resultado;
     }
 
-    public Integer substract(List<Integer> numbers){
-        Integer resultado = numbers.stream()
-                .sorted((x, y) -> y.compareTo(x))
-                .reduce((acumulator, number) -> {
-                    return acumulator - number;
-                }).get();
+    /**
+     * Método para restar los elementos de dos listas del mismo tamaño,
+     * si las listas son de diferente tamaño devuelve un arreglo vacío
+     *
+     * @param list1 primera lista de tipo Double
+     * @param list2 segunda lista de tipo Double
+     * @return devuelve una nueva lista con los valores de cada item sumados
+     */
+    public List<Double> substract(List<Double> list1, List<Double> list2){
+        if(list1.size() != list2.size()){return List.of();}
+        List<Double> resultado = list1.stream()
+                .map(element -> {
+                    Integer index = list1.indexOf(element);
+                    return element - list2.get(index);
+                }).collect(Collectors.toList());
+
         return resultado;
     }
 
-    public Integer multiply(List<Integer> numbers){
-        Integer resultado = numbers.stream()
-                .sorted((x, y) -> x.compareTo(y))
-                .reduce((acumulador, number) -> {
-                    return acumulador * number;
-                }).get();
+    /**
+     * Método para multiplicar los elementos de dos listas del mismo tamaño,
+     * si las listas son de diferente tamaño devuelve un arreglo vacío
+     *
+     * @param list1 primera lista de tipo Double
+     * @param list2 segunda lista de tipo Double
+     * @return devuelve una nueva lista con los valores de cada item sumados
+     */
+    public List<Double> multiply(List<Double> list1, List<Double> list2){
+        if(list1.size() != list2.size()){return List.of();}
+        List<Double> resultado = list1.stream()
+                .map(element -> {
+                    Integer index = list1.indexOf(element);
+                    return element * list2.get(index);
+                }).collect(Collectors.toList());
+
         return resultado;
     }
 
-    public Integer split(List<Integer> numbers){
-        Integer resultado = numbers.stream()
-                .sorted((x, y) -> y.compareTo(x))
-                .reduce((acumulador, number) -> {
-                    return acumulador / number;
-                }).get();
+    /**
+     * Método para dividir los elementos de dos listas del mismo tamaño,
+     * si las listas son de diferente tamaño devuelve un arreglo vacío
+     *
+     * @param list1 primera lista de tipo Double
+     * @param list2 segunda lista de tipo Double
+     * @return devuelve una nueva lista con los valores de cada item sumados
+     */
+    public List<Double> split(List<Double> list1, List<Double> list2){
+        if(list1.size() != list2.size()){return List.of();}
+        List<Double> resultado = list1.stream()
+                .map(element -> {
+                    Integer index = list1.indexOf(element);
+                    if(list2.get(index) == 0D){
+                        return 0D;
+                    }
+                    return element / list2.get(index);
+                }).collect(Collectors.toList());
+
+        return resultado;
+    }
+
+    /**
+     * Método para calcular el módulo de los elementos de dos listas del mismo tamaño,
+     * si las listas son de diferente tamaño devuelve un arreglo vacío
+     *
+     * @param list1 primera lista de tipo Double
+     * @param list2 segunda lista de tipo Double
+     * @return devuelve una nueva lista con los valores de cada item sumados
+     */
+    public List<Double> modul(List<Double> list1, List<Double> list2){
+        if(list1.size() != list2.size()){return List.of();}
+        List<Double> resultado = list1.stream()
+                .map(element -> {
+                    Integer index = list1.indexOf(element);
+                    if(list2.get(index) == 0D){
+                        return 0D;
+                    }
+                    return element % list2.get(index);
+                }).collect(Collectors.toList());
+
         return resultado;
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,32 @@
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Main para la ejecución del programa
+ *
+ * @author Óscar Farfán - oscarfarfan92@gmail.com
+ *
+ * @version 1.0.0.000 7-07-2022
+ */
 public class Main {
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
 
-        List<Integer> numbers1 = Arrays.asList(2, 4, 6, 8, 10);
-        List<Integer> numbers2 = Arrays.asList(1, 3, 5, 7, 9);
+        List<Double> numbers1 = Arrays.asList(8D, 4D, 7D, 11D, 12D);
+        List<Double> numbers2 = Arrays.asList(1D, 2D, 8D, 9D, 5D);
 
-        List<Integer> summedLists = Arrays.asList(calculator.add(numbers1), calculator.add(numbers2));
+        System.out.println("------------------------------------------------");
+        System.out.println("Lista 1: " + numbers1);
+        System.out.println("Lista 2: " + numbers2);
+        System.out.println("------------------------------------------------");
+        System.out.println("Suma: " + calculator.add(numbers1, numbers2));
+        System.out.println("Resta: " + calculator.substract(numbers1, numbers2));
+        System.out.println("Multiplicación: " + calculator.multiply(numbers1, numbers2));
+        System.out.println("División: " + calculator.split(numbers1, numbers2));
+        System.out.println("Modulo: " + calculator.modul(numbers1, numbers2));
 
-        System.out.println("La suma de las listas es: " + calculator.add(summedLists));
-        System.out.println("La resta de las listas es: " + calculator.substract(summedLists));
-        System.out.println("La multiplicación de las listas es: " + calculator.multiply(summedLists));
-        System.out.println("La división de las listas es: " + calculator.split(summedLists));
 
     }
 
